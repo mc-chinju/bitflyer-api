de46c579b0ec35eb8596284d11545f1f58ce9c18
cohki0305 <cohki0305@gmail.com>
2017-11-10 20:08:57 +0900

    exec `bundle gem bitflyer-api`

---
+++ spec/bitflyer/api_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Bitflyer::Api do
+  it "has a version number" do
+    expect(Bitflyer::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
