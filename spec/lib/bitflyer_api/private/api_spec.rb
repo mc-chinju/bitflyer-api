b53cce40c4b5d8dcdf8ad5e446bdc157dcef5b22
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2018-04-08 19:49:25 +0900

    Add Private API test [api.rb]

--- spec/lib/bitflyer_api/private/api_spec.rb
+++ spec/lib/bitflyer_api/private/api_spec.rb
@@ -1,6 +1,20 @@
 require "spec_helper"
 
 RSpec.describe "HTTP Public Api" do
-  it do
+  before do
+    BitflyerApi.configure do |config|
+      config.key    = "dummy_key"
+      config.secret = "dummy_secret"
+    end
+
+    @client = BitflyerApi.client
+  end 
+
+  it "GET my_permissions" do
+    VCR.use_cassette("get_my_permissions") do
+      response = @client.my_permissions
+
+      expect(response.first).to eq "/v1/getmarkets" # representative sample
+    end
   end
 end
