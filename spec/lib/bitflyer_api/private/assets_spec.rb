91bf010447a120b6a8169d8ec83a1ff344b3fbc4
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2018-04-08 22:12:24 +0900

    Add Private API test [assets.rb]

--- spec/lib/bitflyer_api/private/assets_spec.rb
+++ spec/lib/bitflyer_api/private/assets_spec.rb
@@ -1,6 +1,48 @@
 require "spec_helper"
 
 RSpec.describe "HTTP Public Api" do
-  it do
+  before do
+    BitflyerApi.configure do |config|
+      config.key    = "dummy_key"
+      config.secret = "dummy_secret"
+    end
+
+    @client = BitflyerApi.client
+  end 
+
+  it "GET my_balance" do
+    VCR.use_cassette("get_my_balance") do
+      response = @client.my_balance
+      _response = response.first
+
+      aggregate_failures do
+        expect(_response["currency_code"]).to eq "JPY"
+        expect(_response["amount"]).to be >= 0.0
+        expect(_response["available"]).to be >= 0.0
+      end
+    end
+  end
+
+  it "GET my_collateral" do
+    VCR.use_cassette("get_my_collateral") do
+      response = @client.my_collateral
+
+      aggregate_failures do
+        expect(response["collateral"]).to be >= 0.0
+        expect(response["open_position_pnl"]).to be >= 0.0
+        expect(response["require_collateral"]).to be >= 0.0
+        expect(response["keep_rate"]).to be >= 0.0
+      end      
+    end
+  end
+
+  it "GET my_collateral_accounts" do
+    VCR.use_cassette("get_my_collateral_accounts") do
+      response = @client.my_collateral_accounts
+      _response = response.first
+
+      expect(_response["currency_code"]).to eq "JPY"
+      expect(_response["amount"]).to be >= 0.0
+    end
   end
 end
